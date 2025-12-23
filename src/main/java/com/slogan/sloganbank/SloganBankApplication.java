c701b62344f7e19e203f881207689ec5202ec99f
minahosseini <fatemeh.mhosseini11@gmail.com>
2025-12-23 11:09:53 +0330

    Initial Commit

---
+++ src/main/java/com/slogan/sloganbank/SloganBankApplication.java
@@ -0,0 +1,13 @@
+package com.slogan.sloganbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SloganBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SloganBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slogan/sloganbank/SloganbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slogan.sloganbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SloganbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
